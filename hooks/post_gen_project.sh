37f220e6daa6796b5a3a75ae350a124031b9289b
Marc <marc.masdeu@gmail.com>
2018-02-27 16:13:51 +0100

    Forced break after each case.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,7 +3,7 @@ echo "Do you wish create a new GitHub repository?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
-	    scripts/01_create_github_repo.sh || break;;
+	    scripts/01_create_github_repo.sh; break;;
         No ) break;;
     esac
 done
@@ -12,7 +12,7 @@ echo "Do you wish to generate a key for deploying documentation?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
-	    scripts/02_generate_ecdsa_key.sh || break;;
+	    scripts/02_generate_ecdsa_key.sh; break;;
         No ) break;;
     esac
 done
@@ -22,7 +22,7 @@ echo "Do you wish to initialize the GitHub repository?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
-	    scripts/03_initialize_github_repo.sh || break;;
+	    scripts/03_initialize_github_repo.sh; break;;
         No ) break;;
     esac
 done
@@ -31,12 +31,12 @@ echo "Do you wish to upload the key for Travis-CI documentation deployment to Gi
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
-	    scripts/04_upload_deploy_key.sh || break
+	    scripts/04_upload_deploy_key.sh; break
 	    echo "Do you wish to deploy the uploaded key to Travis CI?"
 	    select yn in "Yes" "No"; do
 		case $yn in
 		    Yes )
-			scripts/05_deploy_key_to_travis_ci.sh || break
+			scripts/05_deploy_key_to_travis_ci.sh; break
 			;;
 		    No ) break
 			 ;;
