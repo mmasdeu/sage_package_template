add21f67188bf2dc6d4debca5b7ff77eb331f117
Marc <marc.masdeu@gmail.com>
2020-03-26 09:22:33 +0100

    Update 01_create_github_repo.sh

--- {{cookiecutter.app_name}}/scripts/01_create_github_repo.sh
+++ {{cookiecutter.app_name}}/scripts/01_create_github_repo.sh
@@ -2,7 +2,10 @@
 curl -s https://api.github.com/repos/{{cookiecutter.github_username}}/{{cookiecutter.app_name}} | grep "Not Found" > /dev/null || { echo >&2 "The repository {{cookiecutter.app_name}} already exists in {{cookiecutter.github_username}}.  Aborting."; exit 1; }
 
 echo "Trying to create a new repository on github.com."
-echo "You will be asked for the GitHub password corresponding to the user {{cookiecutter.github_username}}"
+echo "You will be asked for the GitHub password corresponding to the user {{cookiecutter.github_username}}".
+echo "Instead of your password, you MUST use a Personal Authentication Token !!!"
+echo "( See here how to get one:"
+echo "  https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line )"
 echo "{{cookiecutter.github_username}}/{{cookiecutter.app_name}}"
 echo "{{cookiecutter.project_short_description}}"
 
